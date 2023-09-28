fab5a24e1354e6e1ae9821512c0c1acb9805f830
marktos247 <aj.marktos@yahoo.com>
2023-09-28 00:29:33 -0700

    Python Exception

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,6 +8,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / i
-        except: result = b + a
+        except:
+            result = b + a
             break
     return (result)
